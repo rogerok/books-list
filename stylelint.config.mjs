b685a6ba58c72aaec43b4c431ec0c0daab46f0b2
rogerok <fedorik833@gmail.com>
2025-09-27 12:19:02 +0300

    installed stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-clean-order',
+    'stylelint-config-prettier-scss',
+  ],
+};
