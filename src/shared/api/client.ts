59e717ac56903b1f6cd2dec87007d69624fce2bc
vyacheslav <fedorik833@gmail.com>
2025-09-29 17:56:32 +0300

    created supabse client, added env vars validation

---
+++ src/shared/api/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const client = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
+  {},
+);
--- src/shared/config/env/envSchema.ts
+++ src/shared/config/env/envSchema.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
 export const EnvSchema = z.object({
-  VITE_API_URL: z.string({
-    error: 'Не задана переменная окружения VITE_API_URL',
+  VITE_SUPABASE_KEY: z.string({
+    error: 'Не задана переменная окружения VITE_SUPABASE_URL',
+  }),
+  VITE_SUPABASE_URL: z.string({
+    error: 'Не задана переменная окружения VITE_SUPABASE_URL',
   }),
 });
