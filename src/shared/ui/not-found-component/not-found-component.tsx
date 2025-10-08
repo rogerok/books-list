bfd5f39a87f05e95a8e5303662de43eb804170f6
vyacheslav <fedorik833@gmail.com>
2025-10-08 14:26:51 +0300

    update not found layout

--- src/shared/ui/not-found-component/not-found-component.tsx
+++ src/shared/ui/not-found-component/not-found-component.tsx
@@ -2,8 +2,9 @@ import { cn } from '@bem-react/classname';
 import { AppLink } from '@shared/ui/app-link/app-link.tsx';
 import { PageMeta } from '@shared/ui/page-meta/page-meta.tsx';
 import { Typography } from '@shared/ui/typography/typography.tsx';
-import { VStack } from '@shared/ui/vstack/vstack.tsx';
 import { type FC } from 'react';
+
+import './not-found-component.scss';
 
 const cnNotFoundComponent = cn('NotFoundComponent');
 
@@ -17,14 +18,12 @@ export const NotFoundComponent: FC<NotFoundComponentProps> = (props) => {
   return (
     <main className={cnNotFoundComponent(undefined, [className])}>
       <PageMeta title={'Страница не найдена'} />
-      <VStack align={'center'} as={'p'} flexJustify={'center'} gap={'32'}>
-        <Typography align={'center'} as={'h1'} size={'xl'}>
-          Страница не найдена
-        </Typography>
-        <AppLink to={'/'} variant={'outline'}>
-          На главную
-        </AppLink>
-      </VStack>
+      <Typography align={'center'} as={'h1'} size={'xl'}>
+        Страница не найдена
+      </Typography>
+      <AppLink to={'/'} variant={'outline'}>
+        На главную
+      </AppLink>
     </main>
   );
 };
